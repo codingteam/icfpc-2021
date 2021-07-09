3cc9f9fd2dbc7e2cfd4bb41ade0b80a3516b9c93
Akon32 <akon32@rambler.ru>
2021-07-09 21:50:16 +0300

    Add rasterizing SolutionValidator (untested)

--- src/main/scala/org/codingteam/icfpc2021/package.scala
+++ src/main/scala/org/codingteam/icfpc2021/package.scala
@@ -4,16 +4,47 @@ package object icfpc2021 {
 
   case class Point(x: BigInt, y: BigInt) {
     override def toString: String = s"($x, $y)"
+
+    def +(other: Point): Point = Point(this.x + other.x, this.y + other.y)
+
+    def -(other: Point): Point = Point(this.x - other.x, this.y - other.y)
   }
+
+  object Point {
+    val Zero: Point = Point(0, 0)
+    val Ones: Point = Point(1, 1)
+  }
+
+  case class PointD(x: Double, y: Double)
 
   case class Edge(vertex1: Int, vertex2: Int) {
     override def toString: String = s"v$vertex1 - v$vertex2"
   }
+  case class Polygon(vertexes:Vector[Int])
 
   case class Figure(edges: Vector[Edge], vertices: Vector[Point])
 
-  case class Problem(hole: Vector[Point], epsilon: BigInt, figure: Figure)
+  case class Problem(hole: Vector[Point], epsilon: BigInt, figure: Figure) {
+    lazy val holeRect: Rect = Rect(
+      Point(hole.minBy(_.x).x, hole.minBy(_.y).y),
+      Point(hole.maxBy(_.x).x, hole.maxBy(_.y).y))
 
-  case class Solution(problem: Problem, solution: Figure)
+    lazy val holeCenter: PointD = {
+      val xSum = hole.view.map(_.x).sum
+      val ySum = hole.view.map(_.y).sum
+      PointD(xSum.toDouble / hole.size, ySum.toDouble / hole.size)
+    }
+  }
 
+  case class Solution(vertices: Vector[Point])
+
+  /**
+   * Границы 2D
+   *
+   * @param min минимальная точка (включительно).
+   * @param max максимальная точка (включительно).
+   */
+  case class Rect(min: Point, max: Point) {
+    lazy val size: Point = max - min + Point.Ones
+  }
 }
--- src/main/scala/org/codingteam/icfpc2021/validator/SolutionValidator.scala
+++ src/main/scala/org/codingteam/icfpc2021/validator/SolutionValidator.scala
@@ -1,7 +1,74 @@
 package org.codingteam.icfpc2021.validator
 
-import org.codingteam.icfpc2021.Problem;
+import org.codingteam.icfpc2021.{Point, Problem, Solution}
+
+import java.awt.Color
+import java.awt.image.BufferedImage
+import scala.swing.Graphics2D
 
 class SolutionValidator(problem: Problem) {
+  private val ClearColor: Color = Color.BLACK
+  private val FillColor: Color = Color.WHITE
+  private lazy val (shiftX, shiftY, scaleX, scaleY, imgSizeX, imgSizeY) = {
+    require(problem.hole.nonEmpty, "Hole points list must be not empty")
+    val MaxImageSize = 4 * 1024
+    val Point(shiftX, shiftY) = problem.holeRect.min
+    val Point(sizeX, sizeY) = problem.holeRect.size
+    val (imgSizeX, scaleX) = if (sizeX > MaxImageSize)
+      (MaxImageSize, MaxImageSize.toDouble / sizeX.toDouble)
+    else
+      (sizeX.toInt, 1.0)
+
+    val (imgSizeY, scaleY) = if (sizeX > MaxImageSize)
+      (MaxImageSize, MaxImageSize.toDouble / sizeY.toDouble)
+    else
+      (sizeY.toInt, 1.0)
+
+    (shiftX, shiftY, scaleX, scaleY, imgSizeX, imgSizeY)
+  }
+  private lazy val (holeImageArray, figureImage, figureImageArray) = {
+    require(problem.hole.nonEmpty, "Hole points list must be not empty")
+    val figureImage = new BufferedImage(imgSizeX, imgSizeY, BufferedImage.TYPE_INT_RGB)
+    val figureImageArray = Array.ofDim[Int](imgSizeX * imgSizeY)
+
+    val holeImage = new BufferedImage(imgSizeX, imgSizeY, BufferedImage.TYPE_INT_RGB)
+    val g2 = holeImage.getGraphics.asInstanceOf[Graphics2D]
+    g2.setColor(ClearColor)
+    g2.fillRect(0, 0, imgSizeX, imgSizeY)
+    val (holeXs, holeYs, holeLength) = splitPointsIntoCoords(problem.hole map pointToImageCoord)
+    g2.setColor(FillColor)
+    g2.fillPolygon(holeXs, holeYs, holeLength)
+    val holeImageArray = Array.ofDim[Int](imgSizeX * imgSizeY)
+    holeImage.getRGB(0, 0, imgSizeX, imgSizeY, holeImageArray, 0, imgSizeX)
+    (holeImageArray, figureImage, figureImageArray)
+  }
+
+  private def pointToImageCoord(p: Point): Point = {
+    Point(((p.x - shiftX).toDouble * scaleX).toInt, ((p.y - shiftY).toDouble * scaleY).toInt)
+  }
+
+  private def splitPointsIntoCoords(ps: Seq[Point]) = {
+    (ps.view.map(_.x.toInt).toArray, ps.view.map(_.y.toInt).toArray, ps.size)
+  }
+
+  def validate(solution: Solution): Boolean = {
+    val g2 = figureImage.getGraphics.asInstanceOf[Graphics2D]
+    g2.setColor(ClearColor)
+    g2.fillRect(0, 0, imgSizeX, imgSizeY)
+    g2.setColor(FillColor)
+    for (e <- problem.figure.edges) {
+      val v1 = pointToImageCoord(solution.vertices(e.vertex1))
+      val v2 = pointToImageCoord(solution.vertices(e.vertex2))
+      g2.drawLine(v1.x.toInt, v1.y.toInt, v2.x.toInt, v2.y.toInt)
+    }
+    figureImage.getRGB(0, 0, imgSizeX, imgSizeY, figureImageArray, 0, imgSizeX)
+    // compare raster arrays.
+    val clearColorRGB = ClearColor.getRGB
+    for (i <- figureImageArray.indices) {
+      if (figureImageArray(i) != clearColorRGB && holeImageArray(i) == clearColorRGB)
+        return false
+    }
+    true
+  }
 
 }
