63220ecce9298fd6a2843387598131aef98ebed4
Friedrich von Never <friedrich@fornever.me>
2021-07-09 22:30:10 +0700

    Visualizer: show images

--- src/main/scala/org/codingteam/icfpc2021/Model.scala
+++ src/main/scala/org/codingteam/icfpc2021/Model.scala
@@ -3,5 +3,10 @@ package org.codingteam.icfpc2021
 case class Point(x: Int, y: Int) {
   override def toString: String = s"($x, $y)"
 }
-case class Figure(edges: Vector[Point], vertices: Vector[Point])
+
+case class VertexPair(vertex1: Int, vertex2: Int) {
+  override def toString: String = s"v$vertex1 - v$vertex2"
+}
+
+case class Figure(edges: Vector[VertexPair], vertices: Vector[Point])
 case class Problem(hole: Vector[Point], epsilon: Int, figure: Figure)
--- src/main/scala/org/codingteam/icfpc2021/visualizer/Visualizer.scala
+++ src/main/scala/org/codingteam/icfpc2021/visualizer/Visualizer.scala
@@ -2,8 +2,10 @@ package org.codingteam.icfpc2021.visualizer
 
 import org.codingteam.icfpc2021.Json
 
+import java.awt.{Color, Graphics, Graphics2D, Polygon}
 import java.nio.file.{Files, Path}
-import scala.swing.Frame
+import javax.swing.JPanel
+import scala.swing.{Component, Frame}
 
 object Visualizer {
   def show(problemFile: Path): Unit = {
@@ -11,9 +13,25 @@ object Visualizer {
     val problem = Json.parseProblem(content)
 
     val hole = problem.hole
+    val figure = problem.figure
+    val holePoly = new Polygon(hole.map(_.x).toArray, hole.map(_.y).toArray, hole.size)
 
     new Frame {
       title = "visualizer"
+
+      contents = Component.wrap(new JPanel() {
+        override def paint(g: Graphics): Unit = {
+          val g2 = g.asInstanceOf[Graphics2D]
+          g2.draw(holePoly)
+
+          g2.setColor(Color.RED)
+          for (edge <- figure.edges) {
+            val e1 = figure.vertices(edge.vertex1)
+            val e2 = figure.vertices(edge.vertex2)
+            g.drawLine(e1.x, e1.y, e2.x, e2.y)
+          }
+        }
+      })
 
       pack()
       centerOnScreen()
