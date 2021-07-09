0f387b2eb84c70e4cf795d477dd65f82aa478abf
💮 <pink_snow@t.me>
2021-07-09 20:00:38 +0000

    Update aspect ratio calculation

--- src/main/scala/org/codingteam/icfpc2021/visualizer/Translator.scala
+++ src/main/scala/org/codingteam/icfpc2021/visualizer/Translator.scala
@@ -5,30 +5,39 @@ import java.awt.Polygon
 
 /** Translate between model coordinates (BigInt) and pixel screen coordinates (Int). */
 class Translator(problem: Problem) {
-  private val screenBorder = 8
-
-  private var width = 200
-  private var height = 200
 
   private val minX = (problem.hole.map(_.x).min).min(problem.figure.vertices.map(_.x).min)
   private val minY = (problem.hole.map(_.y).min).min(problem.figure.vertices.map(_.y).min)
   private val maxX = (problem.hole.map(_.x).max).max(problem.figure.vertices.map(_.x).max)
   private val maxY = (problem.hole.map(_.y).max).max(problem.figure.vertices.map(_.y).max)
 
-  def setScreenDimensions(width: Int, height: Int): Unit = {
-    this.width = width.max(screenBorder * 2 + 1)
-    this.height = height.max(screenBorder * 2 + 1)
+  private var scale1 = BigInt(1)
+  private var scale2 = BigInt(1)
+  private var shiftX = 0
+  private var shiftY = 0
+
+  def setScreenDimensions(newWidth: Int, newHeight: Int): Unit = {
+    val screenBorder = 8
+
+    val width = newWidth.max(screenBorder * 2 + 1)
+    val height = newHeight.max(screenBorder * 2 + 1)
+
+    if ((width - screenBorder*2) * (maxY - minY) < (height - screenBorder*2) * (maxX - minY)) {
+      scale1 = width - screenBorder * 2
+      scale2 = maxX - minX
+      shiftY = ((height - (maxY - minY) * scale1 / scale2) / 2).toInt
+      shiftX = screenBorder
+    } else {
+      scale1 = height - screenBorder * 2
+      scale2 = maxY - minY
+      shiftX = ((width - (maxX - minX) * scale1 / scale2) / 2).toInt
+      shiftY = screenBorder
+    }
   }
 
   def toScreen(p: Point): (Int, Int) = (toScreenX(p.x), toScreenY(p.y))
-  def scaleToScreen() : BigInt = {
-    val scaleX = (width - screenBorder * 2 ) / (maxX - minX)
-    val scaleY = (height - screenBorder * 2 )  / (maxY - minY)
-    scaleX.min(scaleY)
-  }
-  // TODO: preserve aspect ratio
-  def toScreenX(x: BigInt): Int = ((x - minX) * scaleToScreen() + screenBorder).toInt
-  def toScreenY(y: BigInt): Int = ((y - minY) * scaleToScreen() + screenBorder).toInt
+  def toScreenX(x: BigInt): Int = ((x - minX) * scale1 / scale2).toInt + shiftX
+  def toScreenY(y: BigInt): Int = ((y - minY) * scale1 / scale2).toInt + shiftY
 
   def toModel(x: Int, y: Int): Point = ???
 
