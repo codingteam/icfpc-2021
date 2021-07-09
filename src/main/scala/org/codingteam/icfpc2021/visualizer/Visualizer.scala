970a6a87ce966a4529728fe6f25d6acec9f4b7f1
💮 <pink_snow@t.me>
2021-07-09 21:47:38 +0000

    Draw selection rectangle

--- src/main/scala/org/codingteam/icfpc2021/visualizer/Visualizer.scala
+++ src/main/scala/org/codingteam/icfpc2021/visualizer/Visualizer.scala
@@ -3,7 +3,7 @@ package org.codingteam.icfpc2021.visualizer
 import org.codingteam.icfpc2021.{Json, Point, Problem}
 
 import java.awt._
-import java.awt.event.{MouseEvent, MouseListener}
+import java.awt.event._
 import java.nio.file.{Files, Path}
 import javax.swing.{JPanel, WindowConstants}
 import scala.swing.{Component, Frame}
@@ -15,8 +15,11 @@ object Visualizer {
 
     val translator = new Translator(problem)
 
+    var selection: Option[(java.awt.Point, java.awt.Point)] = None
+
     val panel = new JPanel() {
         override def paint(g: Graphics): Unit = {
+          super.paint(g);
           val g2 = g.asInstanceOf[Graphics2D]
           translator.setScreenDimensions(getWidth, getHeight)
 
@@ -26,6 +29,15 @@ object Visualizer {
           for ((vert, i) <- problem.hole.zipWithIndex) {
             val (x, y) = translator.toScreen(vert)
             g.drawString(String.valueOf(i), x, y)
+          }
+
+          selection match {
+            case Some(r) => {
+              val (x1, x2) = (r._1.x.min(r._2.x), r._1.x.max(r._2.x))
+              val (y1, y2) = (r._1.y.min(r._2.y), r._1.y.max(r._2.y))
+              g2.drawRect(x1, y1, x2 - x1, y2 - y1)
+            }
+            case None => {}
           }
 
           g2.setColor(Color.RED)
@@ -43,14 +55,34 @@ object Visualizer {
         }
       }
     panel.setPreferredSize(new Dimension(600, 400))
+
     panel.addMouseListener(new MouseListener() {
       override def mouseClicked(me: MouseEvent): Unit = {
         println(translator.toModel(me.getX, me.getY))
       }
       override def mouseEntered(e: MouseEvent): Unit = {}
       override def mouseExited(e: MouseEvent): Unit = {}
-      override def mousePressed(e: MouseEvent): Unit = {}
-      override def mouseReleased(e: MouseEvent): Unit = {}
+      override def mousePressed(e: MouseEvent): Unit = {
+        selection = Some((new java.awt.Point(e.getX, e.getY), new java.awt.Point(e.getX, e.getY)))
+        panel.repaint()
+      }
+      override def mouseReleased(e: MouseEvent): Unit = {
+        selection = None
+        panel.repaint()
+      }
+    })
+
+    panel.addMouseMotionListener(new MouseMotionListener() {
+      override def mouseDragged(e: MouseEvent): Unit = {
+        selection match {
+          case Some(sel) => {
+            sel._2.setLocation(e.getX, e.getY)
+            panel.repaint()
+          }
+          case None => {}
+        }
+      }
+      override def mouseMoved(e: MouseEvent): Unit = {}
     })
 
     new Frame {
