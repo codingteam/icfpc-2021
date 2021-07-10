474defac8e523efd87a840f71414d3b128930b9b
💮 <pink_snow@t.me>
2021-07-10 17:15:46 +0000

    Getting things done

--- src/main/scala/org/codingteam/icfpc2021/visualizer/package.scala
+++ src/main/scala/org/codingteam/icfpc2021/visualizer/package.scala
@@ -4,7 +4,6 @@ import java.awt.event.{ActionEvent, KeyEvent}
 import javax.swing.{AbstractAction, Action}
 
 package object visualizer {
-  // TODO: move this helper function somewhere to an appropriate place
   def makeAction(name: String, callback: () => Unit, mnemonic: Option[Char] = None): AbstractAction = {
     val a = new AbstractAction(name) {
       override def actionPerformed(e: ActionEvent): Unit = callback()
