0ee43347b5885e732eaa608556a24da39563d93e
Alexander Batischev <eual.jp@gmail.com>
2021-07-10 14:16:22 +0300

    Add assertions to DumbSolver tests

--- src/test/scala/org/codingteam/icfpc2021/DumbSolverSpec.scala
+++ src/test/scala/org/codingteam/icfpc2021/DumbSolverSpec.scala
@@ -9,22 +9,22 @@ import org.codingteam.icfpc2021._
 import org.codingteam.icfpc2021.solver._
 
 class DumbSolverSpec extends AnyFlatSpec with should.Matchers {
-  "Brezenhem" should "return circle" in {
-    val points = DumbSolver.brezenhem(5)
+  "Brezenhem" should "return circle centered at (0, 0) with radius of 5" in {
+    val points = DumbSolver.brezenhem(5).toSet
 
-    val expected = List()
+    val expected = Set(
+      Point(0, 5), Point(0, -5), Point(5, 0), Point(-5, 0),
+      Point(4, 3), Point(4, -3), Point(-4, 3), Point(-4, -3),
+      Point(3, 4), Point(3, -4), Point(-3, 4), Point(-3, -4))
 
-    for (point <- points) {
-      println(point)
-    }
-    //points should be expected
+    points should contain theSameElementsAs expected
   }
 
   "Triangle" should "be solved" in {
     val p1 = Point(0, 0)
     val p2 = Point(9, 3)
     val (r1, r2) = DumbSolver.calcThirdPoint(p1, p2, 5, 5)
-    println(r1)
-    println(r2)
+
+    Seq(r1, r2) should contain theSameElementsAs Seq(Point(5, 0), Point(4, 3))
   }
 }
