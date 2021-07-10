e3fbd935a6beea743f3190ebc57073290449b897
Alexander Batischev <eual.jp@gmail.com>
2021-07-10 15:07:42 +0300

    Teach DumbSolver.brezenhem about non-integer radii
    For example, the first edge in problem 33 is between (14, 34) and (0,
    0), and has length 26*sqrt(2).

--- src/test/scala/org/codingteam/icfpc2021/DumbSolverSpec.scala
+++ src/test/scala/org/codingteam/icfpc2021/DumbSolverSpec.scala
@@ -5,12 +5,14 @@ import flatspec._
 import matchers._
 
 import java.math.BigInteger
+import math.sqrt
+
 import org.codingteam.icfpc2021._
 import org.codingteam.icfpc2021.solver._
 
 class DumbSolverSpec extends AnyFlatSpec with should.Matchers {
-  "Brezenhem" should "return circle centered at (0, 0) with radius of 5" in {
-    val points = DumbSolver.brezenhem(5).toSet
+  "Brezenhem" should "return a circle with radius of 5" in {
+    val points = DumbSolver.brezenhem(1e-300, 5).toSet
 
     val expected = Set(
       Point(0, 5), Point(0, -5), Point(5, 0), Point(-5, 0),
@@ -18,6 +20,20 @@ class DumbSolverSpec extends AnyFlatSpec with should.Matchers {
       Point(3, 4), Point(3, -4), Point(-3, 4), Point(-3, -4))
 
     points should contain theSameElementsAs expected
+  }
+
+  it should "return circle with non-integer radius" in {
+    val radius = sqrt(99*99 + 1*1)
+    val epsilon = (radius - 99) / 2
+
+    val points = DumbSolver.brezenhem(epsilon, radius).toSet
+
+    points should contain (Point(71, 69))
+    points should contain (Point(91, 39))
+    points should contain (Point(99, 1))
+    // Each of the three points above has 8 variations: 4 variations of sights,
+    // and 2 variations where X and Y are swapped
+    points should have size 24
   }
 
   "Triangle" should "be solved" in {
